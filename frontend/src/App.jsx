9246d5d571e5526d85e1f74fa47ecb8dfa9b6fe3
Villl404303 <putraananta404303@gmail.com>
2026-04-07 19:17:17 +0700

    7 april 2026

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,34 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 import DashboardLayout from './pages/dashboard/DashboardLayout';
+import Main from './pages/dashboard/features/Main';
+import Manpower from './pages/dashboard/features/Manpower';
+import Empty from './pages/dashboard/features/Empty';
 import Layout from './layout/Layout';
 
-// a fucntion to indicate the correct user/role 
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const token = localStorage.getItem('token');
   const userRole = localStorage.getItem('userRole');
-
   if (!token) return <Navigate to="/dashboard" replace />;
-  if (allowedRoles && !allowedRoles.includes(userRole)) {
-    return <Navigate to="/dashboard/main" replace />;
-  }
+  if (allowedRoles && !allowedRoles.includes(userRole)) return <Navigate to="/dashboard/main" replace />;
   return children;
 };
 
 function App() {
   return (
     <Routes>
-      {/* homepage/public routes */}
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+      <Route path="/" element={<Layout />}><Route index element={<Home />} /></Route>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Cost Engineer', 'Site Engineer', 'Management']}><DashboardLayout /></ProtectedRoute>}>
+        <Route path="main" element={<Main />} />8, 
+        {/* Rute Teknis Field Resource [cite: 114, 116, 11120, 122] */}
+        <Route path="manpower" element={<ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Site Engineer']}><Manpower /></ProtectedRoute>} />
+        <Route path="empty" element={<ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Site Engineer']}><Empty /></ProtectedRoute>} />
+        
+        {/* Rute Khusus Budget [cite: 124, 422] */}
+        <Route path="budget" element={<ProtectedRoute allowedRoles={['Project Manager', 'Cost Engineer']}><Empty /></ProtectedRoute>} />
       </Route>
-
-      {/* separate login page(dashboard) */}
-      <Route path="/dashboard" element={<Login />} />
-
-      {/* dashboard and its subroutes handling */}
-      <Route
-        path="/dashboard/*"
-        element={
-          <ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Cost Engineer', 'Site Engineer', 'Management']}>
-            <DashboardLayout />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* catch all pages */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
