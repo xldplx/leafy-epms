23f27ab9ed91600db8ad0b1f036e2eef7798cf62
matt <aldraddd01@gmail.com>
2026-04-17 08:48:51 +0700

    feat: delete home, about, contact pages and make dashboard the only page needed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
+import Login from './pages/Dashboard';
 import DashboardLayout from './pages/dashboard/DashboardLayout';
 import Overview from './pages/dashboard/features/Overview';
 import Manpower from './pages/dashboard/features/Manpower';
 import Empty from './pages/dashboard/features/Empty';
-import Layout from './layout/Layout';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const token = localStorage.getItem('token');
   const userRole = localStorage.getItem('userRole');
-  if (!token) return <Navigate to="/dashboard" replace />;
-  if (allowedRoles && !allowedRoles.includes(userRole)) return <Navigate to="/dashboard/main" replace />;
+  if (!token) return <Navigate to="/login" replace />;
+  if (allowedRoles && !allowedRoles.includes(userRole)) return <Navigate to="/dashboard/overview" replace />;
   return children;
 };
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}><Route index element={<Home />} /></Route>
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      
       <Route path="/dashboard" element={<ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Cost Engineer', 'Site Engineer', 'Management']}><DashboardLayout /></ProtectedRoute>}>
+        <Route index element={<Navigate to="overview" replace />} />
         <Route path="overview" element={<Overview />} />
-        {/* Rute Teknis Field Resource [cite: 114, 116, 11120, 122] */}
+        {/* Rute Teknis Field Resource */}
         <Route path="manpower" element={<ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Site Engineer']}><Manpower /></ProtectedRoute>} />
         <Route path="empty" element={<ProtectedRoute allowedRoles={['Project Manager', 'Planner', 'Site Engineer']}><Empty /></ProtectedRoute>} />
         
-        {/* Rute Khusus Budget [cite: 124, 422] */}
+        {/* Rute Khusus Budget */}
         <Route path="budget" element={<ProtectedRoute allowedRoles={['Project Manager', 'Cost Engineer']}><Empty /></ProtectedRoute>} />
       </Route>
+      
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
